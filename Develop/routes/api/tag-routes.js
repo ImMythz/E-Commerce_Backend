4a7cd69ddeb56d5a34b8e0e4d94c969d32418a57
Nate Worley <immythz@gmail.com>
2021-07-19 00:37:25 -0500

    added route to get all Tags with comments

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,9 +3,35 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    // find all tags
+    // be sure to include its associated Product data
+    const getTags = await Tag.findAll({
+      include: {
+        model: Product,
+      },
+      attributes: [
+        'id',
+        'tag_name',
+      ]
+    })
+
+    if (!getTags){
+      res.status(404).json({
+        message: 'No matching Tags found'
+      })
+      return
+    } else {
+
+      // Successful response
+      res.status(200).json(getTags)
+    }
+  } catch (err) {
+    
+    // Error response
+    res.status(500).json(err)
+  }
 });
 
 router.get('/:id', (req, res) => {
