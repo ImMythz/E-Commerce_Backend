0a218bf469372508434e894cdf044de28e839f72
Nate Worley <immythz@gmail.com>
2021-07-18 22:15:13 -0500

    Added associations in index.js for models with comments

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    foreignKey: 'product_id',
+    model: ProductTag,
+  }
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    foreignKey: 'tag_id',
+    model: ProductTag,
+  }
+})
 
+// Exports the models which allows us to use this throughout our project
 module.exports = {
   Product,
   Category,
