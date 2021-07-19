bccc11eb2db075d03711e7697e07a1873e8a8c19
Nate Worley <immythz@gmail.com>
2021-07-18 21:12:47 -0500

    installed dependancies and created Category class with comments

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,12 +1,25 @@
 const { Model, DataTypes } = require('sequelize');
 
+// Connects to our DB
 const sequelize = require('../config/connection.js');
 
+// Creates a class for a Category
 class Category extends Model {}
 
+// Defines our Category model
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true
+    },
+     category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
@@ -17,4 +30,5 @@ Category.init(
   }
 );
 
+// Exports the Category class which allows us to use this throughout our project
 module.exports = Category;
